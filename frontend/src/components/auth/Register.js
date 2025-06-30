c10e27d39714acb945652fa54c607088dca09a02
RajdeepMahanta <rajdeepmahanta287@gmail.com>
2025-06-30 20:02:43 +0530

    Done finally

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -13,7 +13,7 @@ function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     let inputObj = { username, email, password };
-let url = `${process.env.REACT_APP_BACKEND_URL}/auth/register`;
+    let url = `${process.env.REACT_APP_BACKEND_URL}/api/auth/register`;
 
     try {
       const res = await axios.post(url, inputObj);
