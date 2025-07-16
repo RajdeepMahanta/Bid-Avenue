bee4f42793725ddff170956d31f24319bf2b56d9
RajdeepMahanta <rajdeepmahanta287@gmail.com>
2025-07-17 00:23:01 +0530

    UI Fixes

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -15,40 +15,42 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar">
-      <h1>
-        <Link to="/">Online Auction System</Link>
-      </h1>
-      <ul>
-        {isLoggedIn ? (
-          <>
-            <li>
-              <Link to="/items">Items</Link>
-            </li>
-            <li>
-              <Link to="/items/create">Add Item</Link>
-            </li>
-            <li>
-              <Link to="/items/delete">Delete Item</Link>
-            </li>
-            <li>
-              <button onClick={handleLogout} className="logout-button">
-                Logout
-              </button>
-            </li>
-          </>
-        ) : (
-          <>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+    <div className="navbar-container">
+      <nav className="navbar">
+        <h1>
+          <Link to="/">Bid Avenue</Link>
+        </h1>
+        <ul>
+          {isLoggedIn ? (
+            <>
+              <li>
+                <Link to="/items">Items</Link>
+              </li>
+              <li>
+                <Link to="/items/create">Add Item</Link>
+              </li>
+              <li>
+                <Link to="/items/delete">Delete Item</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout} className="logout-button">
+                  Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
